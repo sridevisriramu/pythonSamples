e511fe834ca92df2aaa438b600b682dd2faa12fe
Sridevi <sridevi.sriram@philips.com>
2019-12-24 00:02:32 +0530

    print functions

--- printFunctions.py
+++ printFunctions.py
@@ -8,3 +8,20 @@ def using_control_again():
 
 print using_control_once()
 print using_control_again()
+
+answer = "'Tis but a scratch!"
+
+def black_knight():
+    if answer == "'Tis but a scratch!":
+        return True
+    else:             
+        return  False      # Make sure this returns False
+
+def french_soldier():
+    if answer == "Go away, or I shall taunt you a second time!":
+        return True
+    else:             
+        return False  # Make sure this returns False
+      
+print black_knight()
+print french_soldier()
