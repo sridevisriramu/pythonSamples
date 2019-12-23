c353f64cb9724985e3ce7ce0e239e203f88c25e1
Sridevi <sridevi.sriram@philips.com>
2019-12-23 14:54:42 +0530

    first python checkin

---
+++ sampleStringOperations.py
@@ -0,0 +1,26 @@
+float_1 = 0.25
+float_2 = 40.0
+
+age = 13
+print "I am " + str(age) + " years old!"
+
+number1 = "100"
+number2 = "10"
+
+string_addition = number1 + number2 
+print string_addition
+#string_addition now has a value of "10010"
+
+int_addition = int(number1) + int(number2)
+print int_addition
+#int_addition has a value of 110
+
+string1 = 3.5
+print(int(string1))
+print(float(string1))
+
+product=(float(float_1))*float_2
+print(product)
+
+big_string="The product was " + str(product)
+print big_string
