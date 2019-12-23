82e34c53855e7d94d713f8eedc12662916d1d021
Sridevi <sridevi.sriram@philips.com>
2019-12-23 15:01:41 +0530

    updated with more basic operations

--- sampleStringOperations.py
+++ sampleStringOperations.py
@@ -24,3 +24,13 @@ print(product)
 
 big_string="The product was " + str(product)
 print big_string
+
+
+skill_completed ="Python Syntax"
+exercises_completed=13
+#The amount of points for each exercise may change, because points don't exist yet
+points_per_exercise=5
+point_total=100
+point_total+=(exercises_completed*points_per_exercise)
+
+print "I got " +str(point_total)+" points!"
