c9c58d5ef71ed81ca4d79578fccf8940a8649a47
Sridevi <sridevi.sriram@philips.com>
2019-12-26 23:53:41 +0530

    loop through list and print

--- loopThorughListItemsAndPrint.py
+++ loopThorughListItemsAndPrint.py
@@ -2,3 +2,10 @@ names = ["Adam","Alex","Mariah","Martine","Columbus"]
 
 for name in names:
  print name
+ 
+ 
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for item in a:
+ if item%2==0:
+    print item
