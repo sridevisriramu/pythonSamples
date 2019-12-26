6a5fc60717680910c0463e1c65f9ae17c29e55f4
Sridevi <sridevi.sriram@philips.com>
2019-12-26 13:33:15 +0530

    added few more files

---
+++ functionWithArguments.py
@@ -0,0 +1,14 @@
+def plane_ride_cost(city):
+ if city == "Charlotte":
+  return 183
+ elif city =="Tampa":
+    print city
+    return 220
+ elif city=="Pittsburgh":
+  return 222
+ elif city=="Los Angeles":
+  return 475
+ else:
+  return "city name not matched"
+
+plane_ride_cost("Tampa")
