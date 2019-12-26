18ca6601c01c337a9e5a0ab4718d8d3f5cf76403
Sridevi <sridevi.sriram@philips.com>
2019-12-26 14:28:40 +0530

    list example

---
+++ lists.py
@@ -0,0 +1,24 @@
+numbers = [5, 6, 7, 8]
+
+print "Adding the numbers at indices 0 and 2..."
+print numbers[0] + numbers[2]
+print "Adding the numbers at indices 1 and 3..."
+# Your code here!
+print numbers[1]+numbers[3]
+
+
+suitcase = [] 
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append("soap")
+suitcase.append("comb")
+suitcase.append("towel")
+suitcase.append("shampoo")
+
+
+
+list_length = len(suitcase) # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
