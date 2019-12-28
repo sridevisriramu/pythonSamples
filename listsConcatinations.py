592e81bc4010647ef15235b2c5619dfeaf601ef3
Sridevi <sridevi.sriram@philips.com>
2019-12-28 16:13:22 +0530

    lists concatination

---
+++ listsConcatinations.py
@@ -0,0 +1,20 @@
+"""
+
+Create a function that joins two lists together.
+
+On line 5, define a function called join_lists that has two arguments, x and y. They will both be lists.
+Inside that function, return the result of concatenating x and y together.
+
+"""
+
+
+m = [1, 2, 3]
+n = [4, 5, 6]
+
+# Add your code here!
+def join_lists(x,y):
+ return x+y
+
+
+print join_lists(m, n)
+# You want this to print [1, 2, 3, 4, 5, 6]
